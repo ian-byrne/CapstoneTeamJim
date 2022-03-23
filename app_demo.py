4347c16a2584e77866e3e125cc93306ef32131a0
Ian Byrne <ianbyrne@ians-mbp-2.attlocal.net>
2022-03-23 18:26:19 -0500

    streamlit demo

---
+++ app_demo.py
@@ -0,0 +1,17 @@
+"""Demo app for streamlit."""
+
+import streamlit as st
+
+st.set_page_config(page_title="CapstoneProject")
+st.write("## Hello, Capstone team JIM!!")
+st.write("Potential for the team to utilize streamlit as a interactive essay.")
+
+
+def main():
+    """Run Main."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
