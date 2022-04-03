c49bb560adb00180eed5d79d4bb29ab919c12446
ian-byrne <ianbyrne@ians-mbp-2.attlocal.net>
2022-04-03 18:53:49 -0500

    allow gitignore to put pickle files in data

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,12 @@ import numpy as np
 import pandas as pd
 
 
-def load_data():
-    """Load data."""
-    pass
+def load_data(to_year):
+    """Load the yearly data from pickle file in the data directory."""
+    if to_year == 2018:
+        return pd.read_pickle("data/dataraw_to2018.pkl")
+    if to_year == 2019:
+        return pd.read_pickle("data/dataraw_2019.pkl")
+    else:
+        print("No specified year, loading through 2019")
+        return pd.read_pickle("data/dataraw_2019.pkl")
