013467ce2e19dd27b04b7ae2af80cf0b9c0e11e1
ian-byrne <ianbyrne@ians-mbp-2.attlocal.net>
2022-04-04 19:42:22 -0500

    update of pytorch and utils funcs

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 
 
-def load_data(to_year):
+def load_data(year):
     """Load the yearly data from pickle file in the data directory."""
-    if to_year == 2018:
+    if year == 2018:
         return pd.read_pickle("data/dataraw_to2018.pkl")
-    if to_year == 2019:
+    if year == 2019:
         return pd.read_pickle("data/dataraw_2019.pkl")
     else:
-        print("No specified year, loading through 2019")
-        return pd.read_pickle("data/dataraw_2019.pkl")
+        print("No specified year, loading through 2018")
+        return pd.read_pickle("data/dataraw_to2018.pkl")
