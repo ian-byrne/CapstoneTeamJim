390faf1f108791a5ce8b84ce478b58dea41f1044
Jenny Ney <72942254+jennyney5@users.noreply.github.com>
2022-04-09 09:48:42 -0400

    added tree_regression.py and pkl/png files

--- JN-scratchpads/tree_regression.py
+++ JN-scratchpads/tree_regression.py
@@ -20,6 +20,7 @@ from plotly.subplots import make_subplots
 import plotly.io as pio
 # render plot in default browser
 pio.renderers.default = 'browser'
+import seaborn as sns
 
 from urllib.request import urlopen
 import json
@@ -29,7 +30,7 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 
 ## read data 
 
-df = pd.read_pickle('fulldataset.pkl')
+df = pd.read_pickle('JN-scratchpads/fulldataset.pkl')
 df = df.sort_values(by=['county_fips', 'year']).reset_index(drop=True)
 
 ## isolate target column
@@ -45,7 +46,7 @@ df = df.dropna()
 
 ## only use counties available in other models
 
-with open('VAR_counties.txt', 'r') as f:
+with open('JN-scratchpads/VAR_counties.txt', 'r') as f:
     lines = f.readlines()
 
 VAR_counties = []
@@ -76,18 +77,16 @@ et = ExtraTreesRegressor(max_depth=15, n_estimators=500, random_state=0).fit(
 filename = 'tree_model1.pkl'
 pickle.dump(et, open(filename, 'wb'))
 
-test_score = et.score(X_test, y_test)
-val_score = et.score(X_val, y_val)
-
-## TODO add charts to show test and validation scores
+test_score = round(et.score(X_test, y_test), 3)
+val_score = round(et.score(X_val, y_val), 3)
 
 ## chart of feature importances
 
 d = {'Feature': X.columns, 'Importance': et.feature_importances_}
-df = pd.DataFrame(d)
-df = df.sort_values(by='Importance', ascending=False).reset_index(drop=True)
+feature_df = pd.DataFrame(d)
+feature_df = feature_df.sort_values(by='Importance', ascending=False).reset_index(drop=True)
 
-fig = px.bar_polar(df.iloc[:30,:], r='Importance', theta='Feature',
+fig = px.bar_polar(feature_df.iloc[:30,:], r='Importance', theta='Feature',
             color='Feature', template='plotly_dark',
             color_discrete_sequence=px.colors.sequential.Plasma_r)
 
@@ -110,10 +109,79 @@ fig.write_image("tree_model1_prediciton_delta.png")
 
 ## calculate rmse
 
-rmse = mean_squared_error(data2019['median_sale_price'], data2019['Predicted_sale_price_change'], squared=False)
+rmse = '$'+str(round(mean_squared_error(data2019['median_sale_price'], data2019['Predicted_sale_price_change'], squared=False)))
 
-## TODO add correlation plot and model removing highly correlated features
 
-print('Model 1 Test Score:', test_score, 
-        'Model 1 Validation Score:', val_score, 
-        'Model 1 RMSE:', rmse)
+fig = go.Figure(data=[go.Table(header=dict(values=['Test R2', '2019 Validation R2', '2019 RMSE']),
+                 cells=dict(values=[[test_score], [val_score], [rmse]]))
+                     ])
+fig.write_image("tree_model1_scores.png")
+
+corr_df = df[['home_value_median', 'median_ppsf', 'median_list_price', 'median_list_ppsf', 'median_sale_price']]
+mask = np.triu(np.ones_like(corr_df.corr(), dtype=np.bool))
+sns.heatmap(corr_df.corr(), mask=mask, vmin=-1, vmax=1, annot=True, cmap='RdPu')
+plt.savefig('tree_model1_correlation_matrix.png')
+
+##TODO alter scale/resolution of images
+## model2 with highly correlated features removed
+
+df = df.drop(['home_value_median', 'median_list_price', 'median_ppsf', 'median_list_ppsf'], axis=1)
+data = df[df['year']!=2019]
+data2019 = df[df['year']==2019]
+
+
+X = data.iloc[:,3:-1]
+y = data.iloc[:,-1]
+
+X_val = data2019.iloc[:,3:-1]
+y_val = data2019.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+## fit best model determined by pyCaret and gridsearch
+
+et = ExtraTreesRegressor(max_depth=15, n_estimators=500, random_state=0).fit(
+       X_train, y_train)
+
+filename = 'tree_model2.pkl'
+pickle.dump(et, open(filename, 'wb'))
+
+test_score = round(et.score(X_test, y_test), 3)
+val_score = round(et.score(X_val, y_val), 3)
+
+## chart of feature importances
+
+d = {'Feature': X.columns, 'Importance': et.feature_importances_}
+feature_df = pd.DataFrame(d)
+feature_df = feature_df.sort_values(by='Importance', ascending=False).reset_index(drop=True)
+
+fig = px.bar_polar(feature_df.iloc[:30,:], r='Importance', theta='Feature',
+            color='Feature', template='plotly_dark',
+            color_discrete_sequence=px.colors.sequential.Plasma_r)
+
+fig.write_image("tree_model2_feature_importances.png")
+
+## chart of prediction delta
+
+pred2019 = et.predict(X_val)
+data2019['Predicted_sale_price_change'] = pred2019
+data2019['Prediction_delta'] = ((data2019['median_sale_price'] - data2019['Predicted_sale_price_change'])/data2019['median_sale_price'])*100
+
+fig = px.choropleth(data2019, geojson=counties, locations='county_fips', color='Prediction_delta',
+                           color_continuous_scale="Viridis",
+                            range_color=(0, 100),
+                           scope="usa",
+                           labels={'Prediction_delta':'Prediction delta for 2019 HPI'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.write_image("tree_model2_prediciton_delta.png")
+
+## calculate rmse
+
+rmse = '$'+str(round(mean_squared_error(data2019['median_sale_price'], data2019['Predicted_sale_price_change'], squared=False)))
+
+
+fig = go.Figure(data=[go.Table(header=dict(values=['Test R2', '2019 Validation R2', '2019 RMSE']),
+                 cells=dict(values=[[test_score], [val_score], [rmse]]))
+                     ])
+fig.write_image("tree_model2_scores.png")
