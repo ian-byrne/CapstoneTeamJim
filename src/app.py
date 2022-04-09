faf125e8803ecf58aaf6fa05d348cf4aa94fe6f2
ian-byrne <ianbyrne@ians-mbp-2.attlocal.net>
2022-04-09 14:50:59 -0500

    added imports to app.py to see if that fixes cloud imports

--- src/app.py
+++ src/app.py
@@ -3,6 +3,31 @@ Main module for display of the Capstone project for Team JIM.
 """
 import streamlit as st
 
+import warnings
+from utils import load_data
+
+# imports for training
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+
+# import dataset, network to train and metric to optimize
+from pytorch_forecasting import (
+    Baseline,
+    TimeSeriesDataSet,
+    TemporalFusionTransformer,
+    QuantileLoss,
+    RMSE,
+    MAE,
+    MAPE,
+    MASE,
+    SMAPE,
+)
+from pytorch_forecasting.data import TimeSeriesDataSet, GroupNormalizer, NaNLabelEncoder
+import torch
+
+warnings.filterwarnings("ignore")  # avoid printing out absolute paths
+
 # the below imports are subject to change based on the final form of modules.
 from model_one import model1
 from model_two import model2
@@ -41,8 +66,9 @@ def main():
 
     if choice == "RNN Analysis":
         st.subheader("Neural Network Analysis")
-        
-        run_nn_model()
+        period = st.radio("Please select yearly or monthly data", ("Yearly", "Monthly"))
+        # display text for post/anaylsis
+        run_nn_model(period)
 
     if choice == "Conclusion":
         st.subheader("Conclusions")
