8f50037fab43385bd70d76dd324778a5daea2aab
ian-byrne <ian@trinitypeg.com>
2022-04-19 09:35:25 -0500

    small model updates

--- streamlit/model_three.py
+++ streamlit/model_three.py
@@ -12,18 +12,17 @@ import plotly.io as pio
 
 
 def pytorch_writeup():
-
+    """Display the pytorch-forecasting write up."""
     with urlopen(
         "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
     ) as response:
         counties = json.load(response)
 
-    preds = pd.read_csv("data/monthly2021_pytorch_pred_allresidential.csv")
+    preds = pd.read_csv("streamlit/data/monthly2021_pytorch_pred_allresidential.csv")
 
     st.header("Temporal Fusion Transformer ")
     st.write("Below is the histogram of residual error")
 
-    # TODO: confirm with melanie how she made her df for his data
     hist = px.histogram(
         preds["target-prediction"], title="Histogram of residual error of model"
     )
@@ -31,16 +30,4 @@ def pytorch_writeup():
     st.plotly_chart(hist, use_container_width=True)
 
     st.write("Below is the predictions error for all counties")
-
-    # Pred2021_error = px.choropleth(preds, geojson=counties, locations='FIPS', color='Forecast error %',
-    #                         color_continuous_scale="Viridis",
-    #                         hover_name = 'County',
-    #                         hover_data =['Predicted Median Sale Price 2020'],
-    #                         scope="usa",
-    #                         labels={'Forecast error %':'2020 % Forecast error'},
-    #                         title = 'Average forecast error by ACS county for 2020 prediction,
-    #                                with most counties having less error than 5%'
-    #                       )
-
-    #st.plotly_chart(Pred2021_error, use_container_width=True)
 
