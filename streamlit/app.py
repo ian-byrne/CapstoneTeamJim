adcb80a6a080ed8f5a72c86508e3a4acf7b4639a
Stelanie <Stelanie@umich.edu>
2022-04-20 08:33:46 -0400

    wording and spacing

--- streamlit/app.py
+++ streamlit/app.py
@@ -80,8 +80,7 @@ if choice == "Home":
     #### Project:
 
     We each selected a model type that interested us and proved to test well on our
-    preliminary data. This led to a time series Vector Autoregression (VAR) model, a supervised ExtraTreesRegressor model, and a neural network Transformer model being used in the project.  
-    We looked at several targets for each region, and concluded that median sale price would be our final prediction target as it's more intuitive for the end user to understand.  As you will see, some models did better or worse depending on the target metric and the time granularity. 
+    preliminary data. This led to a time series Vector Autoregression (VAR) model, a supervised ExtraTreesRegressor model, and a neural network Transformer model being used in the project.  We looked at several targets for each region, and concluded that median sale price would be our final prediction target as it's more intuitive for the end user to understand.  As you will see, some models did better or worse depending on the target metric and the time granularity. 
 
     """
     )
